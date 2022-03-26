c0ae8705831966d16126722b3c03131fe40ce887
railwayproject2022 <100795018+railwayproject2022@users.noreply.github.com>
2022-03-26 19:16:57 +0530

    Add files via upload

---
+++ pro/includes/inc-navbar.php
@@ -0,0 +1,15 @@
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+
+
+            </div>
+            <ul class="nav navbar-nav">
+                 <li class="<?php echo $class != 'reg' ? 'active' : '' ?>">
+                    <a href="index.php">Home</a>
+              
+            </ul>
+        </div>
+
+    </nav>
